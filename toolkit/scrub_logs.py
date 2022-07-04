1f7cc9f93c771fb0805c012c0921c33a8ae70139
Remington Rohel <rar129@usask.ca>
2022-07-04 22:26:42 +0000

    Created a script to scrub all parameters from all log files with a given timestamp.
    * Calls analyze_times.py with the relevant parameters for each file type and data field.

---
+++ toolkit/scrub_logs.py
@@ -0,0 +1,42 @@
+import glob
+import argparse
+from analyze_times import extract_data
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-directory", help="Name of the file to analyze.", required=True)
+    parser.add_argument("-s", "--timestamp", help="Timestamp to identify which log files to scrub. "
+                                                  "Format as YYYY.MM.DD.HH:MM, or truncate to a desired "
+                                                  "precision. All files with this prefix will be analyzed.",
+                        required=True)
+    parser.add_argument("-V", "--version", help="Version of Borealis which generated the log file. Supported"
+                                                "versions are 'v0.5' and 'v0.6'", default='v0.5')
+    parser.add_argument("-d", "--plot_dir", help="Directory to save plots.")
+
+    args = parser.parse_args()
+
+    if args.version not in ['v0.5', 'v0.6']:
+        raise ValueError("Unknown Borealis version")
+
+    if args.plot_dir is not None:
+        plot_dir = args.plot_dir
+    else:
+        plot_dir = args.input_directory
+
+    filetypes_to_datatypes = {
+        'radar_control': ['num_sqns'],
+        'data_write': ['parse_time', 'write_time'],
+        'signal_processing': ['memcpy_time', 'dec_time', 'bf_time', 'corr_time', 'send_time'],
+        'rx_signal_processing': ['memcpy_time', 'processing_time', 'send_time']
+    }
+    print("Globbing {}/{}*".format(args.input_directory, args.timestamp))
+    print(glob.glob('{}/{}*'.format(args.input_directory, args.timestamp)))
+    for file in glob.glob("{}/{}*".format(args.input_directory, args.timestamp)):
+        print(file)
+        filetype = file.split('-')[-1]
+
+        if filetype not in filetypes_to_datatypes.keys():
+            continue
+
+        for data_type in filetypes_to_datatypes[filetype]:
+            extract_data(file, data_type, args.version, plot_dir)
